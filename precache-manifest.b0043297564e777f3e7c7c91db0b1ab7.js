09725973c9f2b1f1c5426dfd324256b01915405d
jatin-rathee <jatinrathee2018@gmail.com>
2020-07-11 15:47:19 +0530

    Updates

---
+++ precache-manifest.b0043297564e777f3e7c7c91db0b1ab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec14899892e20ada0c1f92413ba11e15",
+    "url": "/humans-rolodex/index.html"
+  },
+  {
+    "revision": "d3993abdca03d3c9773b",
+    "url": "/humans-rolodex/static/css/main.6fff621a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/humans-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/humans-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3993abdca03d3c9773b",
+    "url": "/humans-rolodex/static/js/main.d54db356.chunk.js"
+  },
+  {
+    "revision": "8c69258cb6c7d2c198c4",
+    "url": "/humans-rolodex/static/js/runtime-main.f767405d.js"
+  }
+]);
